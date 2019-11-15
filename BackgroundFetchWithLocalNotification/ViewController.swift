1a1436ef007e6d738b5984fbf5d6a527984c2519
Natalie <nwy0206@gmail.com>
2019-11-15 12:19:26 +0800

    Init project

---
+++ BackgroundFetchWithLocalNotification/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BackgroundFetchWithLocalNotification
+//
+//  Created by Natalie Ng on 2019/11/15.
+//  Copyright © 2019 Natalie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
